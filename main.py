00af2a50cd34aa1f97012bcc66156b0dd23be93b
Kastet <63261287+Kastet99@users.noreply.github.com>
2024-04-26 20:53:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ base.metadata.create_all(bind=engine)
 app = FastAPI()
 app.include_router(UserRouter.router, prefix="/user")
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=1407, reload=True, workers=3)
+#if __name__ == "__main__":
+#    uvicorn.run("main:app", host="127.0.0.1", port=1407, reload=True, workers=3)
