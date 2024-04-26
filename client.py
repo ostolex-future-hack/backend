bd17a6d256f91320e1b5a647fdfc93a2c9eeb309
Kastet <63261287+Kastet99@users.noreply.github.com>
2024-04-26 20:51:37 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,5 +1,12 @@
 from openai import OpenAI
+import os
+
+
+API_KEY = os.environ.get('OPENAI_API_KEY')
+
+if not API_KEY:
+    raise ValueError('API key not found')
 
 client = OpenAI(
-    api_key='<your_api_key>'
+    api_key=API_KEY
 )
